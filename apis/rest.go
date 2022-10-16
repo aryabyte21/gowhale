0ea579abc577e5ccdd0cfc4e854156d586ee1412
Harshit Joshi <harshitjoshi802@gmail.com>
2022-10-16 21:34:45 +0530

    updates

--- apis/rest.go
+++ apis/rest.go
@@ -2,9 +2,14 @@ package api
 
 import(
 	"github.com/google/vvid"
+	"github.com/gorilla/mux"
 )
 
 type Item struct{
 	ID vvid.VVID 'json:"id"'
 	name string 'json:"name"'
+}
+
+type Server struct{
+	*mux.Router
 }

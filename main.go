8d5b93faec4d435ccdb4d20868d28a21cabc1597
Harshit Joshi <harshitjoshi802@gmail.com>
2022-10-17 21:56:57 +0530

    nothing but readme commit

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"./database"
 	"./routes"
+	"net/http"
+	"fmt"
 	"github.com/gofiber/fiber"
 	"github.com/gofiber/fiber/middleware/cors"
 )
@@ -17,6 +19,11 @@ func main() {
 	}))
 
 	routes.Setup(app)
-
+	http.HandleFunc("/",googleDef)
 	app.Listen(":8000")
+	http.ListenAndServe(":8000", nil)
+}
+
+func googleDef(w http.ResponseWriter, r *http.Request){
+	fmt.Fprint(w "Haveli me aapka swaagat hai %s", r.URL.Path[1:])
 }
